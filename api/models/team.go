8cd32c5c68b5ca015fbb067c6c2240ece90d03af
Kronborg <mkronborg7@gmail.com>
2023-09-29 11:46:33 +0200

    1.1.3 of database tables

--- api/models/team.go
+++ api/models/team.go
@@ -3,5 +3,5 @@ package models
 type Team struct {
 	ID    uint   `json:"id"    gorm:"primaryKey"`
 	Name  string `json:"name"`
-	Users []User `json:"users" gorm:"many2many:permission_weekdays;"`
+	Users []User `json:"users" gorm:"many2many:team_user;"`
 }
