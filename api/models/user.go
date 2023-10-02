7af228c532afddf7e8356f54bac101f79b6724e5
Kronborg <mkronborg7@gmail.com>
2023-10-02 16:39:24 +0200

    1.2.5 add RHIF to user and made code unique

--- api/models/user.go
+++ api/models/user.go
@@ -7,7 +7,8 @@ type User struct {
 	ID       uint    `json:"id"       gorm:"primaryKey"`
 	Email    string  `json:"email"    gorm:"uniqueIndex;size:191"`
 	Password string  `json:"password"`
-	Code     string  `json:"code"     gorm:"not null;"`
+	Code     string  `json:"code"     gorm:"uniqueIndex"` // not null; MABY NOT NULL
+	RFID     *string `json:"rfid"`
 	Name     string  `json:"name"`
 	Teams    []*Team `                gorm:"many2many:teams_users;"`
 }
