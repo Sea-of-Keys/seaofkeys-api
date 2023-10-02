b55c608af755a1d2026a89aa5b4237498eb0399a
Kronborg <mkronborg7@gmail.com>
2023-10-02 20:39:20 +0200

    made templet for embedde

--- api/repos/embedded.go
+++ api/repos/embedded.go
@@ -1,7 +1,42 @@
 package repos
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/gorm"
 
-type Embeddedhepo struct {
+	"github.com/Sea-of-Keys/seaofkeys-api/api/models"
+)
+
+type EmbeddedRepo struct {
 	db *gorm.DB
+}
+
+func (r *EmbeddedRepo) GetEmbedded(id uint) (*models.Embedded, error) {
+	return nil, nil
+}
+func (r *EmbeddedRepo) GetEmbeddeds() ([]models.Embedded, error) {
+	return nil, nil
+}
+func (r *EmbeddedRepo) PostEmbedded(embed models.Embedded) (*models.Embedded, error) {
+	return nil, nil
+}
+func (r *EmbeddedRepo) PutEmbedded(embed models.Embedded) (*models.Embedded, error) {
+	return nil, nil
+}
+func (r *EmbeddedRepo) DelEmbedded(id uint) (bool, error) {
+	return true, nil
+}
+
+// The Embedded it self
+func (r *EmbeddedRepo) GetSetup(id uint) error {
+	return nil
+}
+func (r *EmbeddedRepo) PostSetup() error {
+	return nil
+}
+func (r *EmbeddedRepo) PostCode(code string) (bool, error) {
+	return true, nil
+}
+
+func NewEmbeddedRepo(db *gorm.DB) *EmbeddedRepo {
+	return &EmbeddedRepo{db}
 }
