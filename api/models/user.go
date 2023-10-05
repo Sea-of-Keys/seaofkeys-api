1af7361f0de2005c339e943ee2afa0bc4f4f9b49
Kronborg <mkronborg7@gmail.com>
2023-10-05 10:35:04 +0200

    change size to 1000 size 6 to small

--- api/models/user.go
+++ api/models/user.go
@@ -8,7 +8,7 @@ type User struct {
 	Name     string  `json:"name"`
 	Email    string  `json:"email"    gorm:"uniqueIndex;size:191"`
 	Password string  `json:"password"`
-	Code     string  `json:"code"     gorm:"uniqueIndex;size:1000"` // not null; MABY NOT NULL
+	Code     string  `json:"code"     gorm:"uniqueIndex;size:100"` // not null; MABY NOT NULL
 	RFID     *string `json:"rfid"`
 	Teams    []*Team `                gorm:"many2many:teams_users;"`
 }
