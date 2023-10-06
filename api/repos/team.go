803afb5dcdad914053359e172261bb70e359c366
Kronborg <mkronborg7@gmail.com>
2023-10-06 18:16:12 +0200

    made crud repo on teams

--- api/repos/team.go
+++ api/repos/team.go
@@ -10,24 +10,42 @@ type TeamRepo struct {
 	db *gorm.DB
 }
 
-func (r *TeamRepo) GetTeam(id uint) (models.Team, error) {
-	return models.Team{}, nil
+func (r *TeamRepo) GetTeam(id uint) (*models.Team, error) {
+	var team models.Team
+	if err := r.db.Debug().First(&team, id).Error; err != nil {
+		return nil, err
+	}
+	return &team, nil
 }
 func (r *TeamRepo) GetTeams() ([]models.Team, error) {
+	var team []models.Team
+	if err := r.db.Debug().Find(&team).Error; err != nil {
+		return nil, err
+	}
+	return team, nil
+}
+func (r *TeamRepo) PostTeam(team models.Team) (*models.Team, error) {
+	if err := r.db.Debug().Create(&team).Error; err != nil {
+		return nil, err
+	}
 	return nil, nil
 }
-func (r *TeamRepo) PostTeam() (models.Team, error) {
-	return models.Team{}, nil
+func (r *TeamRepo) PutTeam(team models.Team) (*models.Team, error) {
+	if err := r.db.Debug().Updates(&team).Error; err != nil {
+		return nil, err
+	}
+	return &team, nil
+}
+func (r *TeamRepo) DelTeam(id uint) (bool, error) {
+	var team models.Team
+	if err := r.db.Debug().Delete(&team, id).Error; err != nil {
+		return false, err
+	}
+	return true, nil
 }
 func (r *TeamRepo) AddToTeam(TeamID, userID uint) (models.Team, error) {
 	return models.Team{}, nil
 }
 func (r *TeamRepo) RemoveFromTeam(TeamID, userID uint) (models.Team, error) {
 	return models.Team{}, nil
-}
-func (r *TeamRepo) PutTeam() (models.Team, error) {
-	return models.Team{}, nil
-}
-func (r *TeamRepo) DelTeam() (bool, error) {
-	return true, nil
 }
