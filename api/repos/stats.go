fb2777d12f9bc85b65426f5346500fb4abdc4c87
Kronborg <mkronborg7@gmail.com>
2023-10-10 16:49:16 +0200

    made main file more readable and made stats endpoint to get user, team, rooms, logins count

--- api/repos/stats.go
+++ api/repos/stats.go
@@ -1,20 +1,48 @@
 package repos
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/gorm"
+
+	"github.com/Sea-of-Keys/seaofkeys-api/api/models"
+)
 
 type StatsRepo struct {
 	db *gorm.DB
 }
 
 func (r *StatsRepo) GetUsersCount() (int, error) {
-	return 0, nil
+	var user models.User
+	var count int64
+	if err := r.db.Debug().Model(&user).Count(&count).Error; err != nil {
+		return 0, nil
+	}
+	return int(count), nil
 }
 func (r *StatsRepo) GetTeamsCount() (int, error) {
-	return 0, nil
+	var team models.Team
+	var count int64
+	if err := r.db.Debug().Model(&team).Count(&count).Error; err != nil {
+		return 0, nil
+	}
+	return int(count), nil
 }
 func (r *StatsRepo) GetRoomsCount() (int, error) {
-	return 0, nil
+	var room models.Room
+	var count int64
+	if err := r.db.Debug().Model(&room).Count(&count).Error; err != nil {
+		return 0, nil
+	}
+	return int(count), nil
 }
 func (r *StatsRepo) GetLoginsCount() (int, error) {
-	return 0, nil
+	var login models.User
+	var count int64
+	if err := r.db.Debug().Model(&login).Count(&count).Error; err != nil {
+		return 0, nil
+	}
+	return int(count), nil
+}
+
+func NewStatsRepo(db *gorm.DB) *StatsRepo {
+	return &StatsRepo{db}
 }
