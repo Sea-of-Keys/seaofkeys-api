477b51f064f53e36615f8f034c39c179cbfbb0bf
Kronborg <mkronborg7@gmail.com>
2023-10-12 15:11:11 +0200

    add a endpoint to add teams to user

--- api/repos/permission.go
+++ api/repos/permission.go
@@ -12,26 +12,26 @@ type PermissionRepo struct {
 
 func (r *PermissionRepo) GetPermission(id uint) (*models.Permission, error) {
 	var permission models.Permission
-	if err := r.db.Debug().First(&permission, id).Error; err != nil {
+	if err := r.db.Debug().Preload("User").Preload("Team").Preload("Room").Preload("Weekdays").First(&permission, id).Error; err != nil {
 		return nil, err
 	}
 	return &permission, nil
 }
 func (r *PermissionRepo) GetPermissions() ([]models.Permission, error) {
 	var permission []models.Permission
-	if err := r.db.Debug().Find(&permission).Error; err != nil {
+	if err := r.db.Debug().Preload("User").Preload("Team").Preload("Room").Preload("Weekdays").Find(&permission).Error; err != nil {
 		return nil, err
 	}
 	return permission, nil
 }
 func (r *PermissionRepo) PostPermission(per models.Permission) (*models.Permission, error) {
-	if err := r.db.Debug().Create(&per).Error; err != nil {
+	if err := r.db.Debug().Preload("User").Preload("Team").Preload("Room").Preload("Weekdays").Create(&per).Error; err != nil {
 		return nil, err
 	}
 	return &per, nil
 }
 func (r *PermissionRepo) PutPermission(per models.Permission) (*models.Permission, error) {
-	if err := r.db.Debug().Model(&per).Updates(&per).Error; err != nil {
+	if err := r.db.Debug().Model(&per).Preload("User").Preload("Team").Preload("Room").Preload("Weekdays").Updates(&per).Error; err != nil {
 		return nil, err
 	}
 	return &per, nil
