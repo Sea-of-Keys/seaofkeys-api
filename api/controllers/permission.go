5677116b1554accdc7ea2f5b34ba4ddf0928d0b6
Kronborg <mkronborg7@gmail.com>
2023-10-12 23:58:09 +0200

    add two endpoints one to get users permission and one to get teams permission

--- api/controllers/permission.go
+++ api/controllers/permission.go
@@ -82,6 +82,32 @@ func (con *PermissionController) DelPermission(c *fiber.Ctx) error {
 		"permission": data,
 	})
 }
+func (con *PermissionController) GetFindUsersPermissions(c *fiber.Ctx) error {
+	id, err := c.ParamsInt("id")
+	if err != nil {
+		return fiber.NewError(fiber.StatusInternalServerError, "C800: "+err.Error())
+	}
+	data, err := con.repo.GetUsersPermissions(uint(id))
+	if err != nil {
+		return fiber.NewError(fiber.StatusInternalServerError, "C800: "+err.Error())
+	}
+	return c.JSON(&fiber.Map{
+		"permissions": data,
+	})
+}
+func (con *PermissionController) GetFindTeamsPermissions(c *fiber.Ctx) error {
+	id, err := c.ParamsInt("id")
+	if err != nil {
+		return fiber.NewError(fiber.StatusInternalServerError, "C800: "+err.Error())
+	}
+	data, err := con.repo.GetTeamsPermissions(uint(id))
+	if err != nil {
+		return fiber.NewError(fiber.StatusInternalServerError, "C800: "+err.Error())
+	}
+	return c.JSON(&fiber.Map{
+		"permissions": data,
+	})
+}
 
 func NewPermissionController(repo *repos.PermissionRepo) *PermissionController {
 	return &PermissionController{repo}
@@ -98,4 +124,6 @@ func RegisterPermissionController(db *gorm.DB, router fiber.Router) {
 	PermissionRouter.Post("/", controller.PostPermission)
 	PermissionRouter.Put("/", controller.PutPermission)
 	PermissionRouter.Delete("/", controller.DelPermission)
+	PermissionRouter.Get("/user/:id", controller.GetFindUsersPermissions)
+	PermissionRouter.Get("/team/:id", controller.GetFindTeamsPermissions)
 }
