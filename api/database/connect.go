9f2defb0b9cbab69509a74cf1bf2b2368580ad06
Kronborg <mkronborg7@gmail.com>
2023-10-16 11:50:18 +0200

    Update connect.go

--- api/database/connect.go
+++ api/database/connect.go
@@ -22,11 +22,11 @@ func Init(database string) (*gorm.DB, error) {
 	case "mysql":
 		dsn := fmt.Sprintf(
 			"%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local",
-			os.Getenv("USER"),
-			os.Getenv("PASSWORD"),
-			os.Getenv("HOST"),
-			os.Getenv("DBPORT"),
-			os.Getenv("DATABASE"),
+			os.Getenv("MYSQLUSER"),
+			os.Getenv("MYSQLPASSWORD"),
+			os.Getenv("MYSQLHOST"),
+			os.Getenv("MYSQLPORT"),
+			os.Getenv("MYSQLDATABASE"),
 		)
 		db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
